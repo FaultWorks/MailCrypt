45bb31e24b9a58399ec1b5e145609ecd07dff87d
kanekotic <alvarojosepl@gmail.com>
2018-05-04 09:02:17 +0200

    commit with inital project that has webview

---
+++ main.js
@@ -0,0 +1,39 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow()
+
+  mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+  }))
+
+  // mainWindow.webContents.openDevTools()
+
+//   console.log(document.querySelector('table .HE td div[role="button"][data-tooltip^="Send"]'))
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
