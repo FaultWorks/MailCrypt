f7d7621cf9b2264f80b1fcb6323dc4788b49baa6
kanekotic <alvarojosepl@gmail.com>
2018-05-07 09:53:09 +0200

    added handle for the click of send

--- inyector.js
+++ inyector.js
@@ -1,8 +1,25 @@
+let buttonBinded = false
+function bindSendButton(){
+    let sendButton = document.querySelector('div[role="button"][data-tooltip^="Send"]')
+    if (!sendButton)
+        return false
+    sendButton.addEventListener("click", function(){
+        // event.preventDefault()
+        // event.stopPropagation()
+        alert("nothing happens")
+        return false
+    }); 
+    return true
+}
+
 
 document.addEventListener('readystatechange', function (state, some) {
     if(document.readyState !== "complete")
         return
     console.log("finished loading")
-    // console.log(document.querySelector('table .HE td div[role="button"][data-tooltip^="Send"]'))
+});
 
+document.addEventListener('DOMNodeInserted', function (state, some) {
+    if(!buttonBinded)
+        buttonBinded = bindSendButton()
 });
